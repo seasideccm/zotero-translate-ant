d8d7fd2f9986db754fa382331555b33a3079fd7e
sscc <2094558239@qq.com>
2024-01-24 13:49:59 +0800

    setting

---
+++ addon/chrome/content/test.js
@@ -0,0 +1,6 @@
+var testSearch = new (function () {
+  this.search = search;
+  function search() {
+    window.alert("show info");
+  }
+})();
--- src/modules/ui/testOpen.ts
+++ src/modules/ui/testOpen.ts
@@ -1,11 +1,5 @@
 import { config } from "../../../package.json";
 export function openWindow() {
-       window.openDialog('chrome://${config.addonRef}/content/test.xhtml', '', 'chrome,centerscreen');
+       const url = `chrome://${config.addonRef}/content/test.xhtml`;
+       window.openDialog(url, '', 'chrome,centerscreen');
 };
-
-
-{
-       window.openDialog('chrome://zotero/content/about.xhtml', 'about', 'chrome,centerscreen');
-}
-D: \devZnote\zotero - batch - translate\build\addon\chrome\content\test.xhtml;
-/D:/devZnote / zotero - batch - translate / build / addon / chrome / content / test.xhtm;
