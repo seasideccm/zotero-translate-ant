eb7c86531b5491f4bfbb282a21a40a919bfeaf2f
sscc <2094558239@qq.com>
2024-01-30 08:21:01 +0800

    style: delete comment

--- typings/zotero.d.ts
+++ typings/zotero.d.ts
@@ -2,11 +2,9 @@
 
 declare namespace Zotero {
   interface Promise<T = void> extends _ZoteroTypes.Bluebird<T> {
-    //method(fn: Function): () => _ZoteroTypes.Bluebird<T>;
-    //defer(): _ZoteroTypes.DeferredPromise<T>;
     coroutine(
       generatorFunction: (...args: any[]) => any,
-      options?: { yieldHandler: any },
+      options?: { yieldHandler: any; },
     ): () => Promise<any>;
   }
 }
