2e5a2a24247f005f6fcac7b794e8fdc7e399cf89
sscc <2094558239@qq.com>
2024-04-28 11:29:05 +0800

    0428

--- src/modules/menu.ts
+++ src/modules/menu.ts
@@ -2,18 +2,13 @@ import { config } from "../../package.json";
 import { getString } from "../utils/locale";
 import { getPref } from "../utils/prefs";
 import { arrToObj } from "../utils/tools";
-import { clearAllTable } from "./database/database";
-import { openAddonPrefPane, openAddonShortcut } from "./preferenceScript";
-import { getDom, makeClickButton, makeId, makeTagElementProps } from "./ui/uiTools";
+import { openAddonPrefPane } from "./preferenceScript";
+import { makeClickButton, makeId, makeTagElementProps } from "./ui/uiTools";
 import { decryptFileSelected, encryptFile } from "./crypto";
-import { readTextFiles } from "./ui/tableSecretKeys";
 import { aITransUI, showTrans } from "./ui/dataDialog";
-import { openAIOllama } from "./largeLanguageModels/oneApi";
 import { fullTextTranslate } from "./translate/fullTextTranslate";
 
 function getParasArrs() {
-  const text =
-    "Mechanical ventilation is frequently needed in patients with cardiogenic shock. The aim of this review is to summarize and discuss the current evidence and the pathophysiological mechanism that a clinician should consider while setting the ventilator.";
   const parasArrs = [
     [
       //["读取文件内容", readTextFiles],
@@ -34,7 +29,6 @@ function getParasArrs() {
   return parasArrs;
 }
 export function mountMenu() {
-
   let menu = document.querySelector(`#${makeId("menu")}`);
   if (!menu) {
     const menuProps = makeTagElementProps({
@@ -77,13 +71,8 @@ export function mountMenu() {
     menu.append(menuPopup);
   }
   const location = getPref("addonMenuLocation") || "right";
+
   let ref;
-
-
-
-
-
-
   if (Zotero.isMac) {
     ref = document.querySelector("#zotero-tb-tabs-menu");
     ref?.parentElement?.insertBefore(menu, ref);
@@ -98,80 +87,10 @@ export function mountMenu() {
     }
   }
 
-
-
   function menuitemObj(argsArr: any[]) {
     return arrToObj(["label", "func", "args", "accesskey"], argsArr);
     //"acceltext" 可显示，无功能
   }
-}
-
-export function rightClickMenuItem_ss() {
-  const menuIcon = `chrome://${config.addonRef}/content/icons/favicon@0.5x.png`;
-  ztoolkit.Menu.register("item", {
-    tag: "menuseparator",
-  });
-  // item menuitem with icon
-  ztoolkit.Menu.register("item", {
-    tag: "menuitem",
-    label: getString("menuitem-note") + " ALT+N",
-    commandListener: (ev) => {
-      //Translator.translateFT("note");
-    },
-    icon: menuIcon,
-  });
-  ztoolkit.Menu.register("item", {
-    tag: "menuitem",
-    label: getString("menuitem-pdf") + " ALT+P",
-    commandListener: (ev) => {
-      // Translator.translateFT("pdf");
-    },
-    icon: menuIcon,
-  });
-  ztoolkit.Menu.register("item", {
-    tag: "menuitem",
-    label: getString("menuitem-pdf2Note"),
-    commandListener: async (ev) => {
-      // await this.pdf2Note();       //Translator.pdf2Note();
-    },
-    icon: menuIcon,
-  });
-
-  ztoolkit.Menu.register("item", {
-    tag: "menuitem",
-    label: "添加图片注释",
-    commandListener: async (ev) => {
-      //await imageToAnnotation();       //Translator.pdf2Note();
-    },
-    icon: menuIcon,
-  });
-  ztoolkit.Menu.register("item", {
-    tag: "menuitem",
-    label: "测试保存图片",
-    commandListener: async (ev) => {
-      //await testSaveImg();
-    },
-    icon: menuIcon,
-  });
-
-  ztoolkit.Shortcut.register("event", {
-    id: `${config.addonRef}-key-translate-note`,
-    key: "N",
-    // 似乎只支持单个修饰键
-    //modifiers="accel,shift,alt"
-    modifiers: "alt",
-    callback: (keyOptions) => {
-      addon.hooks.onShortcuts("translateNote");
-    },
-  });
-  ztoolkit.Shortcut.register("event", {
-    id: `${config.addonRef}-key-translate-pdf`,
-    key: "P",
-    modifiers: "alt",
-    callback: (keyOptions) => {
-      addon.hooks.onShortcuts("translatePDF");
-    },
-  });
 }
 
 export function rightClickMenuItem() {
@@ -200,7 +119,6 @@ export function rightClickMenuItem() {
     tag: "menuitem",
     label: getString("menuitem-pdf2Note"),
     commandListener: async (ev) => {
-      //@ts-ignore xxx
       await fullTextTranslate.pdf2Note(); //fullTextTranslate.pdf2Note();
     },
     icon: menuIcon,
